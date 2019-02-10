c8456058289a7eaa995691fbed41602197b7920e
reisman <30975669+reisman@users.noreply.github.com>
2019-02-10 13:45:09 +0100

    Cube waves cleanup

--- cube-wave/main.js
+++ cube-wave/main.js
@@ -1,34 +1,50 @@
+const w = 50;
+const boxes = [];
+
 let angle = 0;
-let w = 30;
 let magicAngle;
-let maxDistance;
+let halfWidth, halfHeight;
+let minBoxHeight, maxBoxHeight;
 
 function setup() {
-    createCanvas(800, 800, WEBGL);
+    createCanvas(900, 900, WEBGL);
+    
+    noStroke();
     normalMaterial();
+    ortho(-700, 700, 700, -700, 0, 1400);
+
     magicAngle = atan(1 / sqrt(2));
-    maxDistance = dist(0, 0, 0.5 * width, 0.5 * height);
+    minBoxHeight = 0.1 * height;
+    maxBoxHeight = 0.6 * height;
+    halfWidth = 0.5 * width;
+    halfHeight = 0.5 * height;
+    const maxDistance = dist(0, 0, halfWidth, halfHeight);
+    
+    for (let z = 0; z < height; z += w) {
+        for (let x = 0; x < width; x += w) {
+            let d = dist(x, z, halfWidth, halfHeight);
+            let offset = map(d, 0, maxDistance, -PI, PI);
+            boxes.push({ x, z, offset });
+        }
+    }
 }
 
 function draw() {
     background(0);
-    ortho(-700, 700, 700, -700, 0, 1400);
     orbitControl();
 
     rotateX(0.25 * PI);
     rotateY(magicAngle);
-
-    for (let z = 0; z < height; z += w) {
-        for (let x = 0; x < width; x += w) {
-            push();
-            let d = dist(x, z, 0.5 * width, 0.5 * height);
-            let offset = map(d, 0, maxDistance, -PI, PI);
-            let a = angle + offset;
-            const h = floor(map(sin(a), -1, 1, 100, 500));
-            translate(x - 0.5 * width, 0, z - 0.5 * height);
-            box(w - 2, h, w - 2);
-            pop();
-        }
+    translate(-halfWidth, 0, -halfHeight);
+    
+    for (b of boxes) {
+        const { x, z, offset } = b;
+        const currentAngle = sin(angle + offset);
+        const boxHeight = map(currentAngle, -1, 1, minBoxHeight, maxBoxHeight);
+        push();
+        translate(x, 0, z);
+        box(w, boxHeight, w);
+        pop();
     }
 
     angle -= 0.1;
